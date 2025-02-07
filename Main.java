9f903011028b7a0146475d5f8c66c9b32f641d42
RasmussBrodin <126715286+RasmussBrodin@users.noreply.github.com>
2025-02-07 11:33:51 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+
+    // used to start the CI server in command line
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello world");
+    }
+}
